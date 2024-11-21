b2385ceffa1b06505b3cd2aee9f8c425abc8c6e5
Hitesh Jadhav <jadhavhitesh207@gmail.com>
2024-11-21 23:23:17 +0530

    updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ['module:react-native-dotenv', {
-      moduleName: '@env',
-      path: '.env',
-    }],
+    ['@babel/plugin-transform-class-properties', {loose: false}],
+    ['@babel/plugin-transform-private-methods', {loose: false}],
+    ['@babel/plugin-transform-private-property-in-object', {loose: false}],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
   ],
 };
